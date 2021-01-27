21c567bcc2d2a47547bb8a672c4da226fe6bb098
Nazar.M <inazar.m@gmail.com>
2021-01-27 23:31:34 +0500

    data is successfully added into database by using form serialize method of jquery and post complete form to controller and save into database and returen value into object of data in json show into table.

--- addDataWithFormSerialize/Controllers/HomeController.cs
+++ addDataWithFormSerialize/Controllers/HomeController.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using addDataWithFormSerialize.Models;
 namespace addDataWithFormSerialize.Controllers
 {
     public class HomeController : Controller
     {
+        employeeDBEntities db = new employeeDBEntities();
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult SaveData(EmployeeInfo employeeInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EmployeeInfoes.Add(employeeInfo);
+                db.SaveChanges();
+                return Json(new {data=employeeInfo },JsonRequestBehavior.AllowGet);
+            }
+
+            return View(employeeInfo);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
